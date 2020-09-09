7534732cadec62dadd5a759ffe69bc0a086177b5
DishitaLumba <69897366+DishitaLumba@users.noreply.github.com>
2020-09-09 14:33:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,13 +13,13 @@ function preload()
 function setup() {
 	createCanvas(800, 700);
 
-    engine = Engine.create();
+        engine = Engine.create();
 	world = engine.world;
 
 	ground_sprite = createSprite(400,660,800,20);
 	ground_sprite.shapeColor = color(255,255,0);
 
-    base_Sprite=createSprite(600, 640, 100,20);
+        base_Sprite=createSprite(600, 640, 100,20);
 	base_Sprite.shapeColor=color(255);
 
 	leftSide_Sprite= createSprite(550,600,20,100);
@@ -31,10 +31,12 @@ function setup() {
 	base = Bodies.rectangle(600, 640, 100, 20, {isStatic:true} );
 	World.add(world,base);
 
-	paper1 = new Paper(20,200,20);
+	paper1 = new Paper(20,640,20);
 	paper1.shapeColor= color(0,205,255);
-     keyPressed();
-
+        keyPressed();
+        
+	paper1.collide(ground_sprite);
+	
 	Engine.run(engine);
   
 }
